9a40a89692c03a46af6dd5c2c914b9a744b6bc34
dhs02 <59893400+dhs02@users.noreply.github.com>
2020-02-04 15:12:48 +0100

    Miniopdrachten

--- Proberen.java
+++ Proberen.java
@@ -1,2 +1,26 @@
-Hallo
-Lijn 2
+class Hallo{
+	public static void main(String[] args){
+		new Fiets().hoi();
+		String voorbeeld = "fred";
+		String upper = voorbeeld.toUpperCase();
+		System.out.println(upper);
+		String lower = voorbeeld.toLowerCase();
+		System.out.println(lower);
+	}
+}
+
+class Koe{
+	void hoi(){
+		System.out.println("oeps");
+	}
+}
+
+class Fiets{
+	void hoi(){
+		System.out.println("ai");
+	}
+}
+
+
+
+

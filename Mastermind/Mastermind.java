4cbf57943166d709e15f51f249ef86dfd3515917
dhs02 <59893400+dhs02@users.noreply.github.com>
2020-02-11 23:44:07 +0100

    Mastermind

--- Mastermind/Mastermind.java
+++ Mastermind/Mastermind.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Mastermind {
 	public static void main(String[] args) {
 		Game game = new Game();
-		System.out.println("Welkom bij Mastermind! Druk op \"s\" om te beginnen.");
+		System.out.println("Welkom bij Mastermind!");
 		game.play();
 		System.out.println("Bedankt voor het spelen!");
 	}
